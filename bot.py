67dcefddff0847015571e66f4468ba7c9e5e8434
Rimk <rimk.main.0@gmail.com>
2026-06-17 20:46:17 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,20 +4,20 @@ import os
 import re
 import urllib3
 
-# Silencia la publicidad molesta en la terminal
+# Silencia la advertencia molesta en la terminal
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # CONFIGURACIÓN DE SERVIDORES
 SERVIDORES = [
     {
-        "url": os.getenv('DISCORD_WEBHOOK'),
+        "url": os.getenv ('DISCORD_WEBHOOK'),
         "color": 14614410
     }
 ]
 
 URL_BCV = "https://www.bcv.org.ve/"
 
-def limpiar_y_formato(texto):
+def limpiar_y_formatear(texto):
     try:
         solo_numeros = re.sub(r'[^0-9,.]', '', texto)
         if ',' in solo_numeros and '.' in solo_numeros:
@@ -30,38 +30,38 @@ def limpiar_y_formato(texto):
 
 def obtener_tasas():
     try:
-        encabezados = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'}
-        respuesta = requests.get(URL_BCV, headers=encabezados, verify=False)
-        sopa = BeautifulSoup(respuesta.content, 'html.parser')
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'}
+        response = requests.get(URL_BCV, headers=headers, verify=False)
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-        d_raw = sopa.find(id="dolar").get_text()
-        e_raw = sopa.find(id="euro").get_text()
-        fecha = sopa.find(class_="fecha-display-single").get_text().strip()
+        d_raw = soup.find(id="dolar").get_text()
+        e_raw = soup.find(id="euro").get_text()
+        fecha = soup.find(class_="date-display-single").get_text().strip()
 
-        dolar = limpiar_y_formato(d_raw)
-        euro = limpiar_y_formato(e_raw)
+        dolar = limpiar_y_formatear(d_raw)
+        euro = limpiar_y_formatear(e_raw)
 
-        for servidor in SERVIDORES:
-            if servidor["url"]:
-                enviar_a_discord(servidor["url"], servidor["color"], dolar, euro, fecha)
+        for server in SERVIDORES:
+            if server["url"]:
+                enviar_a_discord(server["url"], server["color"], dolar, euro, fecha)
                 
     except Exception as e:
         print(f"Error general: {e}")
 
 def enviar_a_discord(url, color, dolar, euro, fecha):
-    carga_util = {
+    payload = {
         "embeds": [{
             "title": "🏛️ BCV | Tasa de Cambio Oficial",
             "description": f"📅 **Fecha:** {fecha}\n",
             "color": color,
             "fields": [
                 {
-                    "name": "**💵 Dólar (USD)**",
+                    "name": "💵 Dólar (USD)",
                     "value": "**```diff\n+ " + dolar + " Bs.\n```**", 
                     "inline": True
                 },
                 {
-                    "name": "**💶 Euro (EUR)**",
+                    "name": "💶 Euro (EUR)",
                     "value": "**```yaml\n" + euro + " Bs.\n```**", 
                     "inline": True
                 }
@@ -72,7 +72,7 @@ def enviar_a_discord(url, color, dolar, euro, fecha):
                 "icon_url": "https://cdn.discordapp.com/attachments/1424141770245275760/1516958980088397914/SiluetaLogonegro.png?ex=6a3489a4&is=6a333824&hm=e820737f3cc6aa5c36f25eadd81f401dc908c958f7678ec37e2d3f44a8238391&"}
         }]
     }
-    requests.post(url, json=carga_util)
+    requests.post(url, json=payload)
 
 if __name__ == "__main__":
     obtener_tasas()
